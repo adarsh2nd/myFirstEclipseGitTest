589216b2a5de4d60257c096351b9b21c9d995897
Adarsh Shrivastava <sadarsh96@gmail.com>
2022-11-27 16:12:30 +0530

    My First commit

---
+++ src/test/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author ADARSH SHRIVASTAVA
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is my first Project");
+
+	}
+
+}
